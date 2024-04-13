68272f727e10913a11ddcf53cf1e605e27110a9c
cristiancmach <cristianc1099@gmail.com>
2024-04-13 13:51:36 -0500

    use if in avatar-tsx

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -17,13 +17,17 @@ const Avatar = () => {
 
         // Agregar listeners de eventos al montar el componente
         const avatarImage = document.getElementById("avatar-image");
-        avatarImage.addEventListener("mouseenter", handleMouseEnter);
-        avatarImage.addEventListener("mouseleave", handleMouseLeave);
+        if (avatarImage) {
+            avatarImage.addEventListener("mouseenter", handleMouseEnter);
+            avatarImage.addEventListener("mouseleave", handleMouseLeave);
+        }
 
         // Remover listeners de eventos al desmontar el componente (Al eliminar los event listeners cuando ya no son necesarios, se optimiza el rendimiento de la aplicación al liberar recursos que de otro modo estarían ocupados por event listeners inútiles.)
         return () => {
-            avatarImage.removeEventListener("mouseenter", handleMouseEnter);
-            avatarImage.removeEventListener("mouseleave", handleMouseLeave);
+            if (avatarImage) {
+                avatarImage.removeEventListener("mouseenter", handleMouseEnter);
+                avatarImage.removeEventListener("mouseleave", handleMouseLeave);
+            }
         };
     }, []);
 
